f973c402988c779044790dd9261f8c361082854b
vinicius-hora <43853758+vinicius-hora@users.noreply.github.com>
2020-02-14 21:45:07 -0300

    aula 14/02

---
+++ atividades de hipermidias/aula14-02/magica.js
@@ -0,0 +1,12 @@
+  <p>Seu browser não entende scripts</p>
+  <a href="exemplo.com.br/no-js">Versão sem scripts</a>
+  var span = document.getElementByid('magia');
+    span.addEventLister('onkeypres'digitar'false');
+
+
+
+  function digitar(){
+    var txt = document.getElementByid('texto').value;
+
+    document.write("<p>" +txt+ "</p>" );
+  }
---
+++ atividades de hipermidias/aula14-02/ocultar.js
@@ -0,0 +1,7 @@
+var elan1 = document.getElementByid('conte1');
+elan1.addEventLister('onclik'conteudo1,'false');
+
+function conteudo1(){
+  var conte1 = document.getElementByid('conte1');
+  conte1.style.display = "none";
+}
