0200a8900702f8412c1d7d0af231baaa4ad0ba7d
vinicius-hora <43853758+vinicius-hora@users.noreply.github.com>
2020-02-18 09:59:12 -0300

    Update ocultar.js

--- atividades de hipermidias/aula14-02/ocultar.js
+++ atividades de hipermidias/aula14-02/ocultar.js
@@ -1,7 +1,21 @@
-var elan1 = document.getElementByid('conte1');
-elan1.addEventLister('onclik'conteudo1,'false');
 
-function conteudo1(){
-  var conte1 = document.getElementByid('conte1');
-  conte1.style.display = "none";
+enla1 = document.getElementById('enlace_1');
+enla2 = document.getElementById('enlace_2');
+enla3 = document.getElementById('enlace_3');
+
+function conteudo(enla){
+  if (enla.target.textContent == 'Ocultar Conteúdo'){
+    content = document.getElementById('conteudo_' + enla.target.id.substring(7));
+    content.style.display = 'none';
+    enla.target.textContent = 'Mostrar Conteúdo';
+  }
+  else{
+    content = document.getElementById('conteudo_' + enla.target.id.substring(7));
+    content.style.display = 'block';
+    enla.target.textContent = 'Ocultar Conteúdo';
+  }
 }
+
+enla1.addEventListener('click',conteudo,false);
+enla2.addEventListener('click',conteudo,false);
+enla3.addEventListener('click',conteudo,false);
